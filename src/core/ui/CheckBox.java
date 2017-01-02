bfc95b37a51e1783e156ff3254e10f720b522b8c
Avoduhcado <lostking9@yahoo.com>
2017-01-02 15:10:06 -0500

    UI: Draw checkbox for checkbox ui elements
    Task-Desc: Checkbox no longer extends from Button.
    Checkboxes will now draw a box next to their text to signify toggleable
    status.

--- src/core/ui/CheckBox.java
+++ src/core/ui/CheckBox.java
@@ -1,18 +1,78 @@
 package core.ui;
 
+import java.awt.Dimension;
+import java.awt.geom.Rectangle2D;
+
+import org.lwjgl.util.vector.Vector3f;
+
+import core.render.DrawUtils;
 import core.ui.event.ActionEvent;
+import core.ui.event.ActionListener;
 import core.ui.event.MouseEvent;
 import core.ui.event.MouseListener;
+import core.ui.event.UIEvent;
 import core.ui.utils.Accessible;
+import core.ui.utils.HasText;
+import core.utilities.ValueSupplier;
+import core.utilities.text.Text;
+import core.utilities.text.TextModifier;
 
-public class CheckBox extends Button implements Accessible {
+public class CheckBox extends UIElement implements HasText, Accessible {
 
-	private boolean checked;
+	protected Dimension checkSize = new Dimension(16, 16);
+	
+	protected boolean checked;
+	protected String text;
+	
+	private ActionListener actionListener;
 	
 	public CheckBox(String text) {
-		super(text);
-		
+		this.text = text;
+		setBounds(0, 0,
+				text != null ? Text.getDefault().getWidth(text) : 1,
+				text != null ? Text.getDefault().getHeight(text) : 1);
+				
 		addMouseListener(new DefaultCheckBoxAdapter());
+	}
+	
+	@Override
+	public void draw() {
+		super.draw();
+
+		DrawUtils.setColor(new Vector3f(1, 1, 1));
+		if(checked) {
+			DrawUtils.fillRect(1, 1, 1, 1, new Rectangle2D.Double(uiBounds.getX(), uiBounds.getY() + (getBounds().getHeight() - checkSize.getHeight()) / 2,
+					checkSize.getWidth(), checkSize.getHeight()));
+		} else {
+			DrawUtils.drawRect(new Rectangle2D.Double(uiBounds.getX(), uiBounds.getY() + (getBounds().getHeight() - checkSize.getHeight()) / 2,
+					checkSize.getWidth(), checkSize.getHeight()));
+		}
+		
+		if(text != null) {
+			Text.drawString(text, getBounds().getX() + checkSize.getWidth(), getBounds().getY(), getTextModifier());
+		}
+	}
+
+	@Override
+	public void access(boolean accessed) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasFocus() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public TextModifier getTextModifier() {
+		return new TextModifier();
+	}
+
+	@Override
+	public void setBounds(ValueSupplier<Double> x, ValueSupplier<Double> y, ValueSupplier<Double> width, ValueSupplier<Double> height) {
+		uiBounds.setFrame(x, y, () -> width.getValue() + checkSize.getWidth(), height);
 	}
 	
 	public boolean isChecked() {
@@ -23,6 +83,33 @@ public class CheckBox extends Button implements Accessible {
 		this.checked = checked;
 	}
 
+	public void removeActionListener(ActionListener l) {
+		if(l == null) {
+			return;
+		}
+		
+		actionListener = null;
+	}
+	
+	public void addActionListener(ActionListener l) {
+		this.actionListener = l;
+	}
+	
+	@Override
+	public void fireEvent(UIEvent e) {
+		super.fireEvent(e);
+		
+		if(e instanceof ActionEvent) {
+			processActionEvent((ActionEvent) e);
+		}
+	}
+	
+	protected void processActionEvent(ActionEvent e) {
+		if(actionListener != null) {
+			actionListener.actionPerformed(e);
+		}
+	}
+
 	/**
 	 * Handle the default actions for mouse events on a button
 	 */
@@ -30,9 +117,6 @@ public class CheckBox extends Button implements Accessible {
 
 		public void mouseClicked(MouseEvent e) {
 			checked = !checked;
-			if(checked) {
-				textColor = "white";
-			}
 			CheckBox.this.fireEvent(new ActionEvent());
 		}
 
@@ -45,15 +129,9 @@ public class CheckBox extends Button implements Accessible {
 		}
 		
 		public void mouseEntered(MouseEvent e) {
-			textColor = "lightGray";
 		}
 		
 		public void mouseExited(MouseEvent e) {
-			if(!checked) {
-				textColor = "gray";
-			} else {
-				textColor = "white";
-			}
 		}
 	}
 
